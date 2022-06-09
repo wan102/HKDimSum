cf1d3689e99603d5aa5dc829a1558cbc176247ce
wan102 <102154201+wan102@users.noreply.github.com>
2022-06-09 19:46:54 +0800

    create map

--- app/src/main/java/com/example/hkdimsum/MapsFragment_test.kt
+++ app/src/main/java/com/example/hkdimsum/MapsFragment_test.kt
@@ -1,27 +1,20 @@
-package com.example.hkdimsum.ui.home
+package com.example.hkdimsum
+
+import androidx.fragment.app.Fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.hkdimsum.R
-import com.example.hkdimsum.databinding.FragmentHomeBinding
+
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class HomeFragment : Fragment() {
-
-    private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+class MapsFragment_test : Fragment() {
 
     private val callback = OnMapReadyCallback { googleMap ->
         /**
@@ -42,29 +35,13 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-//        val textView: TextView = binding.textHome
-//        homeViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
-        return root
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View? {
+        return inflater.inflate(R.layout.fragment_maps_test, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
