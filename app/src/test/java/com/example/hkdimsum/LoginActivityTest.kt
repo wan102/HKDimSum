17b5cb323a51b58fd7627ddb0b719e7b8d7dd073
wan102 <102154201+wan102@users.noreply.github.com>
2022-06-13 00:31:18 +0800

    testcase

--- app/src/test/java/com/example/hkdimsum/LoginActivityTest.kt
+++ app/src/test/java/com/example/hkdimsum/LoginActivityTest.kt
@@ -1,7 +1,10 @@
 package com.example.hkdimsum
 
-import org.junit.Assert.assertEquals
+import android.content.Context
+import android.content.SharedPreferences
+import org.junit.Assert.*
 import org.junit.Test
+import org.mockito.Mockito
 
 class LoginActivityTest {
 
@@ -16,8 +19,36 @@ class LoginActivityTest {
             "user1@gmail.com"
         val editTextPassword =
             "000000"
-        val result = editTextEmailAddress + editTextPassword
+        val email = editTextEmailAddress.toString()
+        val password = editTextPassword.toString()
+        val result = email + password
         assertEquals(result, editTextEmailAddress + editTextPassword)
+    }
+
+    @Test
+    fun performLogin() {
+
+        val sharedPrefs = Mockito.mock(SharedPreferences::class.java)
+        val sharedPrefsEditor = Mockito.mock(SharedPreferences.Editor::class.java)
+        val context = Mockito.mock(Context::class.java)
+
+        //simulate the behaviour of the objects
+        Mockito.`when`(context.getSharedPreferences(Mockito.anyString(), Mockito.anyInt()))
+            .thenReturn(sharedPrefs)
+        Mockito.`when`(sharedPrefs.edit()).thenReturn(sharedPrefsEditor)
+        Mockito.`when`(sharedPrefsEditor.putString(Mockito.anyString(), Mockito.anyString()))
+            .thenReturn(sharedPrefsEditor)
+
+        val email = "user1@gmail.com"
+        val preKey = "LOGINEMAIL_KEY"
+        val preKey2 = "PASSWORD_KEY"
+
+        val login = LoginActivity(email)
+        login.performLogin(email)
+        //verify is the method call
+        Mockito.verify(sharedPrefsEditor).putString(Mockito.argThat { key -> key == preKey },
+            Mockito.argThat { value -> value == email })
+        Mockito.verify(sharedPrefsEditor).commit()
     }
 
     @Test
